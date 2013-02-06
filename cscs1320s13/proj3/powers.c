ccb20fbf4baa99db70d712008e5be8c1efa660d9
Shawn Meas <smeas@lab46.corning-cc.edu>
2013-02-06 10:16:44 -0500

    Added sysprog

--- cscs1320s13/proj3/powers.c
+++ cscs1320s13/proj3/powers.c
@@ -12,21 +12,28 @@ int main(int argc, char *argv[])
 
 	printf("%d %d\n",base,power);
 
-	for ( j = 1; j < power; j++)
+	if (power == 0)
 	{
-		//printf("Loop 1: %d\n",result);
-
-		for (i = 1; i < base; i++)
+		result = 1;
+	}
+	else
+	{
+		for ( j = 1; j < power; j++)
 		{
-			//printf("Loop 2: %d\n",result);
+			//printf("Loop 1: %d\n",result);
 
-			for (k = 0; k < newbase; k++)
+			for (i = 1; i < base; i++)
 			{
-				result++;
-				//printf("Loop 3: %d\n",result);
-			}	
+				//printf("Loop 2: %d\n",result);
+
+				for (k = 0; k < newbase; k++)
+				{
+					result++;
+					//printf("Loop 3: %d\n",result);
+				}	
+			}
+		newbase = result;
 		}
-	newbase = result;
 	}
 
 	printf("The number %d raised to the power %d is %d\n",base,power,result);
