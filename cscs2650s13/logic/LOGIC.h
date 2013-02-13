a554fbe1b7975ec844f7baa89e2639d066ea5a77
Shawn <shawn@shawn-OptiPlex-745.(none)>
2013-02-13 09:24:58 -0500

    Updated LOGIC.h

--- cscs2650s13/logic/LOGIC.h
+++ cscs2650s13/logic/LOGIC.h
@@ -100,28 +100,36 @@ char XOR(input1, input2)
 
 char HALFSUM(input1, input2)
 {
-	char result;
+	char sum;
 
-	result = XOR(input1, input2);
+	sum = XOR(input1, input2);
 
-	return result;
+	return sum;
 }
 
 char HALFCARRY(input1, input2)
 {
-	char result;
+	char carry;
 	
-	result = AND(input1, input2);
+	carry = AND(input1, input2);
 
-	return result;
+	return carry;
 }
 
 char FULLSUM(input1, input2, input3)
 {
-	
+	char sum;
+
+	sum = XOR(XOR(input1, input2), input3);
+
+	return sum;
 }
 
 char FULLCARRY(input1, input2, input3)
 {
+	char carry;
 
+	carry = OR(AND(XOR(input1, input2), input3), AND(input1, input2);
+
+	return carry;
 }
