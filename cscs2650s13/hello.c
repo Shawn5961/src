c4f7b800a6427668619ed8b979c797fefcc0f78e
Shawn <shawn@shawn-OptiPlex-745.(none)>
2013-02-15 12:39:55 -0500

    Fixed some junk

---
+++ cscs2650s13/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	char name [80];
+
+	printf("What is your name?\n");
+	scanf("%s",name);
+	printf("So, your name is %s. That's pretty cool\n",name);
+
+	return 0;
+}
