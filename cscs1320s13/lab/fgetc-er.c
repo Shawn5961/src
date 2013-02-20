51ad2f259b343fe3e55bd83510965b6c865b94cc
Shawn <shawn@shawn-OptiPlex-745.(none)>
2013-02-20 16:24:49 -0500

    Added pointerarrays2

--- cscs1320s13/lab/fgetc-er.c
+++ cscs1320s13/lab/fgetc-er.c
@@ -21,5 +21,24 @@ int main()
 
 	fprintf(stdout, "Your entered ASCII chars %hhu, %hhu, %hhu, and %hhu\n", a, b, c, d);
 
+	if (a >= 48 && a < 57)
+	{	
+		a = a - 48;
+	}
+	if (b >= 48 && b < 57)
+	{	
+		b = b - 48;
+	}
+	if (c  >= 48 && c < 57)
+	{	
+		c = c - 48;
+	}
+	if (d >= 48 && d < 57)
+	{	
+		d = d - 48;
+	}
+	
+	fprintf(stdout, "Your entered numbers are %hhu, %hhu, %hhu, and %hhu\n", a, b, c, d);
+
 	return 0;
 }
