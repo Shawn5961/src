bdedaa77c101ece93eab4843762c31cd83fdfa9b
Shawn <shawn@shawn-OptiPlex-745.(none)>
2013-02-20 13:19:27 -0500

    Finished sdltut3

--- sysprog/sdltut3.cpp
+++ sysprog/sdltut3.cpp
@@ -7,7 +7,7 @@ const int SCREEN_HEIGHT = 480;
 const int SCREEN_BPP = 32;
 
 SDL_Surface *image = NULL;
-SDL_Surface * screen = NULL;
+SDL_Surface *screen = NULL;
 
 SDL_Event event;
 
@@ -39,4 +39,77 @@ void apply_surface( int x, int y, SDL_Surface* source, SDL_Surface* destination 
 	SDL_BlitSurface( source, NULL, destination, &offset );
 }
 
+bool init()
+{
+	if(SDL_Init(SDL_INIT_EVERYTHING) == -1)
+	{
+		return false;
+	}
 
+	screen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_SWSURFACE);
+
+	if(screen == NULL)
+	{
+		return false;
+	}
+
+	SDL_WM_SetCaption("Event test", NULL);
+
+	return true;
+}
+
+bool load_files()
+{
+	image = load_image("hello.bmp");
+
+	if(image == NULL)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+void clean_up()
+{
+	SDL_FreeSurface(image);
+
+	SDL_Quit();
+}
+
+int main(int argc, char* args[])
+{
+	bool quit = false;
+
+	if(init() == false)
+	{
+		return 1;
+	}
+
+	if(load_files() == false)
+	{
+		return 1;
+	}
+
+	apply_surface(0, 0, image, screen);
+
+	if(SDL_Flip(screen) == -1)
+	{
+		return 1;
+	}
+
+	while(quit == false)
+	{
+		while(SDL_PollEvent(&event))
+		{
+			if(event.type ==SDL_QUIT)
+			{
+				quit = true;
+			}
+		}
+	}
+
+	clean_up();
+
+	return 0;
+}
