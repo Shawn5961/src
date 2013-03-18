a6b53ca91d15760206160ef5270a504afd774cd2
shawn <shawn@shawn-U56E.(none)>
2013-03-18 10:04:32 -0400

    (CSCS1320) Work on logic.c

--- cscs1320s13/logic.c
+++ cscs1320s13/logic.c
@@ -66,6 +66,69 @@ char OR(char input1, char input2)
 	return result;
 }
 
+char NAND(char input1, char input2)
+{
+	char result;
+	
+	result = NOT(AND(input1, input2));
+
+	return result;
+}
+
+char NOR(char input1, char input2)
+{
+	char result;
+	
+	result = NOT(OR(input1, input2));
+
+	return result;
+}
+
+char XOR(char input1, char input2)
+{
+	char result;
+	
+	result = OR(AND(NOT(input1), input2), AND(input1, NOT(input2)));
+
+	return result;
+}
+
+char HALFSUM(char input1, char input2)
+{
+	char sum;
+
+	sum = XOR(input1, input2);
+
+	return sum;
+}
+
+char HALFCARRY(char input1, char input2)
+{
+	char carry;
+	
+	carry = AND(input1, input2);
+
+	return carry;
+}
+
+char FULLSUM(char input1, char input2, char input3)
+{
+	char sum;
+
+	sum = XOR(XOR(input1, input2), input3);
+
+	return sum;
+}
+
+char FULLCARRY(char input1, char input2, char input3)
+{
+	char carry;
+
+	carry = OR(AND(XOR(input1, input2), input3), AND(input1, input2));
+
+	return carry;
+}
+
 
 
 int main()
@@ -82,4 +145,48 @@ int main()
 	printf("1 OR 0: %d\n", OR(1, 0));
 	printf("0 OR 1: %d\n", OR(0, 1));
 	printf("0 OR 0: %d\n", OR(0, 0));
+
+	printf("\n1 NOR 1: %d\n", NOR(1, 1));
+	printf("1 NOR 0: %d\n", NOR(1, 0));
+	printf("0 NOR 1: %d\n", NOR(0, 1));
+	printf("0 NOR 0: %d\n", NOR(0, 0));
+
+	printf("\n1 NAND 1: %d\n", NAND(1, 1));
+	printf("1 NAND 0: %d\n", NAND(1, 0));
+	printf("0 NAND 1: %d\n", NAND(0, 1));
+	printf("0 NAND 0: %d\n", NAND(0, 0));
+
+	printf("\n1 XOR 1: %d\n", XOR(1, 1));
+	printf("1 XOR 0: %d\n", XOR(1, 0));
+	printf("0 XOR 1: %d\n", XOR(0, 1));
+	printf("0 XOR 0: %d\n", XOR(0, 0));
+
+	printf("\n1 HALFSUM 1: %d\n", HALFSUM(1, 1));
+	printf("1 HALFSUM 0: %d\n", HALFSUM(1, 0));
+	printf("0 HALFSUM 1: %d\n", HALFSUM(0, 1));
+	printf("0 HALFSUM 0: %d\n", HALFSUM(0, 0));
+
+	printf("\n1 HALFCARRY 1: %d\n", HALFCARRY(1, 1));
+	printf("1 HALFCARRY 0: %d\n", HALFCARRY(1, 0));
+	printf("0 HALFCARRY 1: %d\n", HALFCARRY(0, 1));
+	printf("0 HALFCARRY 0: %d\n", HALFCARRY(0, 0));
+
+	printf("\n1 FULLSUM 1 CARRY 1: %d\n", FULLSUM(1, 1, 1));
+	printf("1 FULLSUM 1 CARRY 0: %d\n", FULLSUM(1, 1, 0));
+	printf("1 FULLSUM 0 CARRY 1: %d\n", FULLSUM(1, 0, 1));
+	printf("1 FULLSUM 0 CARRY 0: %d\n", FULLSUM(1, 0, 0));
+	printf("0 FULLSUM 1 CARRY 1: %d\n", FULLSUM(0, 1, 1));
+	printf("0 FULLSUM 1 CARRY 0: %d\n", FULLSUM(0, 1, 0));
+	printf("0 FULLSUM 0 CARRY 1: %d\n", FULLSUM(0, 0, 1));
+	printf("0 FULLSUM 0 CARRY 0: %d\n", FULLSUM(0, 0, 0));
+	
+	printf("\n1 FULLCARRY 1 CARRY 1: %d\n", FULLCARRY(1, 1, 1));
+	printf("1 FULLCARRY 1 CARRY 0: %d\n", FULLCARRY(1, 1, 0));
+	printf("1 FULLCARRY 0 CARRY 1: %d\n", FULLCARRY(1, 0, 1));
+	printf("1 FULLCARRY 0 CARRY 0: %d\n", FULLCARRY(1, 0, 0));
+	printf("0 FULLCARRY 1 CARRY 1: %d\n", FULLCARRY(0, 1, 1));
+	printf("0 FULLCARRY 1 CARRY 0: %d\n", FULLCARRY(0, 1, 0));
+	printf("0 FULLCARRY 0 CARRY 1: %d\n", FULLCARRY(0, 0, 1));
+	printf("0 FULLCARRY 0 CARRY 0: %d\n", FULLCARRY(0, 0, 0));
+	
 }
