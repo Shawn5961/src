f5f18dcb84daac31c40cf8aecb737c8308902155
Shawn Meas <shawn@highwind.(none)>
2013-03-26 21:06:38 -0400

    (CSCS2650) More work on converting logic lib to C++

--- schoolwork/cscs2650s13/logic/LOGIC.hpp
+++ schoolwork/cscs2650s13/logic/LOGIC.hpp
@@ -3,11 +3,18 @@
 #define INVALID 'x' 
 
 class Logic{
-	char in1, in2, result;
+	char in1, in2, in3, result, sum, carry, s, r, q, qbar;
 public:
 	char AND(char, char);
 	char OR(char, char);
 	char NOT(char);
+	char NAND(char, char);
+	char NOR(char, char);
+	char XOR(char, char);
+	char HALFSUM(char, char);
+	char HALFCARRY(char, char);
+	char FULLSUM(char, char, char);
+	char FULLCARRY(char, char, char);
 };
 
 char Logic::AND(char in1, char in2)
@@ -90,4 +97,53 @@ char Logic::NOT(char in1)
 	}
 
 	return result;
+}
+
+char Logic::NAND(char in1, char in2)
+{
+	result = NOT(AND(in1, in2);
+
+	return result;
+}
+
+char Logic::NOR(char in1, char in2)
+{
+	result = NOT(OR(in1, in2);
+
+	return result;
+}
+
+char Logic::XOR(char in1, char in2)
+{
+	result = OR(AND(NOT(in1), in2), AND(in1, NOT(in2)));
+
+	return result;
+}
+
+char Logic::HALFSUM(char in1, char in2)
+{
+	sum = XOR(in1, in2);
+
+	return sum;
+}
+
+char Logic::HALFCARRY(char in1, char in2)
+{
+	carry = AND(in1, in2);
+
+	return carry;
+}
+
+char Logic::FULLSUM(char in1, char in2, char in3)
+{
+	sum = XOR(XOR(in1, in2), in3);
+
+	return sum;
+}
+
+char Logic::FULLCARRY(char in1, char in2, char in3)
+{
+	carry = OR(AND(XOR(in1, in2), in3), AND(in1, in2));
+
+	return carry;
 }
