3675478e74ca00dbbb7db1ec52fee750dcbe433b
Shawn Meas <shawn@highwind.(none)>
2013-03-27 00:35:11 +0000

    Cleanup

--- src/schoolwork/cscs2650s13/proj2/STATES.h
+++
@@ -1,9 +0,0 @@
-#ifndef __STATES_H
-#define __STATES_H
-
-#include <stdio.h>
-
-char trueState;
-char falseState;
-
-#endif
--- src/schoolwork/unix/Experiment 2/2.sh
+++
@@ -1,2 +0,0 @@
-echo -e "This is a file to create a script"
-echo -e "It will take this script file, and create a new script named one number higher"
