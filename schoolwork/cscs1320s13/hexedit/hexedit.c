15b908bfa6aa7176c0f53e8c34898871b8ac6497
Shawn Meas <shawn.meas89@gmail.com>
2013-04-22 10:36:37 -0400

    (CSCS1320) Work on hex editor

--- schoolwork/cscs1320s13/hexedit/hexedit.c
+++ schoolwork/cscs1320s13/hexedit/hexedit.c
@@ -3,11 +3,13 @@
 
 int main(int argc, char *argv[])
 {
-	char character[80];
+	char buffer[256];
 	FILE *file = fopen(argv[1], "r");
-	int c;
+	int option;
 	int i = 0;
+	int lineNum = 0;
 
+//Todo: Edit value, save file, open file, new buffer, next, previous
 
 	if (argc != 2)
 	{
@@ -15,25 +17,64 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		character[i] = fgetc(file);
 		do
 		{
-			if(character[i] == '\n' || character[i] == ' ')
-			{	
-				printf("%x\n", character[i]);
-				i++;
-				character[i] = fgetc(file);
-			}
-			else
+			printf("\nEnter a command.\n");
+			printf("1: Display next buffer\n");
+			printf("2: Quit\n");
+			scanf("%d", &option);
+			
+			switch (option)
 			{
-				printf("%x ", character[i]);
-				i++;
-				character[i] = fgetc(file);
+				case 1:
+					buffer[i] = fgetc(file);
+					if(buffer[1] != EOF)
+					{
+						printf("     00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15\n");
+						printf("%03d0 ", lineNum);
+						do
+						{
+							if((i % 16) == 15)
+							{	
+								lineNum++;
+								if(lineNum < 16)
+								{
+									printf("%02x\n%03d0 ", buffer[i], lineNum);
+								}
+								else
+								{
+									printf("%02x\n", buffer[i]);
+								}
+								i++;
+								buffer[i] = fgetc(file);
+							}
+							else
+							{
+								printf("%02x ", buffer[i]);
+								i++;
+								buffer[i] = fgetc(file);
+							}
+						} while(buffer[i] != EOF && i < 256);
+						i = 0;
+						lineNum = 0;
+					}
+					else
+					{
+						printf("You have reached the end of the file\n");
+						printf("Please run the program again\n");
+						option = 2;
+					}
+					break;
+				case 2:
+					break;
+				default:
+					printf("Invalid option. Please enter a valid option\n");
+					break;
 			}
-		} while(character[i] != EOF);
-
-		fclose(file);
-
-		return 0;
+		} while(option != 2);
 	}
+	fclose(file);
+
+	return 0;
+	
 }
