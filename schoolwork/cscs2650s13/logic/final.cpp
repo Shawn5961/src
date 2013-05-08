9f21a7cabcab3b6d20cf38fae17958542d3c5007
Shawn Meas <shawn.meas89@gmail.com>
2013-05-08 11:50:41 -0400

    (CSCS2650) More cleanup of final

--- schoolwork/cscs2650s13/logic/final.cpp
+++ schoolwork/cscs2650s13/logic/final.cpp
@@ -4,34 +4,37 @@
 int main()
 {
 	char a = '1', b = '1', c = '0', d = '0', e;
-	Logic logicTester;
+	Logic logic;
 	
 	printf("NOT, AND, OR\n");
-	e = logicTester.NOT(logicTester.AND(b, logicTester.OR(c, a)));
+	e = logic.NOT(logic.AND(b, logic.OR(c, a)));
 	printf("%c\n", e);
 
 	//My RSLatch accepts arguments in the form of S, R
 	printf("\nRSLatch\n");
 	printf("S, R\n");
-	logicTester.RSLATCH(e, b);
-	printf("0, 1: %c\n", logicTester.getQ());
-	logicTester.RSLATCH(d, a);
-	printf("0, 1: %c\n", logicTester.getQ());
-	logicTester.RSLATCH(a, d);
-	printf("1, 0: %c\n", logicTester.getQ());
+	logic.RSLATCH(e, b);
+	printf("0, 1: %c\n", logic.getQ());
+	logic.RSLATCH(d, a);
+	printf("0, 1: %c\n", logic.getQ());
+	logic.RSLATCH(a, d);
+	printf("1, 0: %c\n", logic.getQ());
 
 	//Reset Q's for Flip Flop
-	logicTester.setQs('0', '1');
-	logicTester.setQs2('0', '1');
+	printf("\nSetting Q values\n");
+	logic.setQs('0', '1');
+	printf(" q: %c,  qbar: %c\n", logic.getQ(), logic.getQBar());
+	logic.setQs2('0', '1');
+	printf("q2: %c, qbar2: %c\n", logic.getQ2(), logic.getQBar2());
 
 	printf("\nJK Flip Flop\n");
 	printf("J, K, Clk\n");
-	logicTester.JKFLIPFLOP(a, b, c);
-	printf("1, 1, 0: %c\n", logicTester.getQ2());
-	logicTester.JKFLIPFLOP(b, a, b);
-	printf("1, 1, 1: %c\n", logicTester.getQ2());
-	logicTester.JKFLIPFLOP(a, a, d);
-	printf("1, 1, 0: %c\n", logicTester.getQ2());
-	logicTester.JKFLIPFLOP(b, b, a);
-	printf("1, 1, 1: %c\n", logicTester.getQ2());
+	logic.JKFLIPFLOP(a, b, c);
+	printf("1, 1, 0: %c\n", logic.getQ2());
+	logic.JKFLIPFLOP(b, a, b);
+	printf("1, 1, 1: %c\n", logic.getQ2());
+	logic.JKFLIPFLOP(a, a, d);
+	printf("1, 1, 0: %c\n", logic.getQ2());
+	logic.JKFLIPFLOP(b, b, a);
+	printf("1, 1, 1: %c\n", logic.getQ2());
 }
