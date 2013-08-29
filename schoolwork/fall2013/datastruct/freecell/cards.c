7bad10a396bcad86ad115a17fa86d1f23d4c3ff3
Shawn Meas <shawn.meas89@gmail.com>
2013-08-29 13:42:46 -0400

    Added cards.c

---
+++ schoolwork/fall2013/datastruct/freecell/cards.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+int cardVal[52];
+char cardSuit[52];
+
+int deckInit()
+{
+	int cardNum;
+	char suit;
+
+	int i, j, k = 0;
+
+	for ( i = 0; i < 4; i++ )
+	{
+		switch (i)
+		{
+			case 0:
+				suit = 'c';
+				break;
+
+			case 1:
+				suit = 'd';
+				break;
+
+			case 2:
+				suit = 'h';
+				break;
+
+			case 3:
+				suit = 's';
+				break;
+		}
+		
+		for ( j = 1; j <= 13; j++ )
+		{
+			cardVal[k] = j;
+			cardSuit[k] = suit;
+			k++;
+		}
+	}
+
+/*	for ( k = 0; k < 52; k++ )
+	{
+		printf("Card %d: %d%c\n", k+1, cardVal[k], cardSuit[k]);
+	}
+*/
+
+	return 0;
+}
+
+int main()
+{
+	srand(time(NULL));
+	deckInit();
+
+	int cardPick;
+	int cardFlag[52];
+
+	struct shuffledDeck{
+		int value[52];
+		char suit[52];
+	};
+
+	int i, j, k;
+
+	struct shuffledDeck deck;
+
+	for ( i = 0; i < 52; i++ )
+	{
+		cardFlag[i] = 0;
+	}
+
+	for ( j = 0; j < 52; j++ )
+	{
+		cardPick = rand() % 52;
+
+		if ( cardFlag[cardPick] == 0 )
+		{
+			deck.value[j] = cardVal[cardPick];
+			deck.suit[j] = cardSuit[cardPick];
+			cardFlag[cardPick] = 1;
+		}
+		else
+		{
+			j--;
+		}
+	}
+
+	for ( k = 0; k < 52; k++ )
+	{
+		printf("Card %d: %d%c\n", k+1, deck.value[k], deck.suit[k]);
+	}
+
+	return 0;
+}
